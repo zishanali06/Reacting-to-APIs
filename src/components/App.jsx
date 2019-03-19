70e9a76527b81f9a804761ab10595204b8cf1773
Zishan Ali <zishanali06@gmail.com>
2019-03-19 18:22:59 -0500

    working on lab

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+    return (
+        <div className="container">
+            <div className="row text-center"><h1>Reacting To Api's</h1></div>
+        </div>
+    )
+}
+
+export default App;
